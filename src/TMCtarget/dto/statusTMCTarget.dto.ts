3ff366aae9afe560c2c4c9b5a3a765ce4dd0f2aa
navidul <hnavidul@gmail.com>
2025-06-24 17:19:26 +0600

    Refactor code structure for improved readability and maintainability

--- src/TMCtarget/dto/statusTMCTarget.dto.ts
+++ src/TMCtarget/dto/statusTMCTarget.dto.ts
@@ -1,8 +1,11 @@
 import { TargetStatus } from "@prisma/client";
-import { IsOptional, IsString } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 
 export class StatusTMCTargetDto {
     @IsString()
     @IsOptional()
+    @Transform(({ value }) => value.toUpperCase())
+    @IsEnum(TargetStatus, { message: 'Status must be pending, active, partiallyActive, queued or completed' })
     readonly status?: TargetStatus
 }
